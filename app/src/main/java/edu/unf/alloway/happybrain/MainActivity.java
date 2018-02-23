d2e971905acc3de07e8021107ca308bf4a42a4f6
Cameron Cuff <dev.ctcuff@gmail.com>
2018-02-23 15:59:42 -0500

    Updated code

--- app/src/main/java/edu/unf/alloway/happybrain/MainActivity.java
+++ app/src/main/java/edu/unf/alloway/happybrain/MainActivity.java
@@ -2,12 +2,21 @@ package edu.unf.alloway.happybrain;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // setContentView(R.layout.activity_main);
+
+        // Load the template for now
+        setContentView(R.layout.template_post);
+
+        // Makes the link in the text view clickable
+        TextView webLink = findViewById(R.id.web_link);
+        webLink.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
